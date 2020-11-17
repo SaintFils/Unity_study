4b565d61ccb4cc4df4b53f80cb4681e660a897fa
Oleg <gurmanoleg@yandex.ru>
2020-11-17 05:13:01 +0300

    Homework
    Homework Unity project added

---
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/MoveFirst.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MoveFirst : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3;
+    private Vector3 _direction = Vector3.zero; 
+
+     private void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+
+        transform.position += _direction * Time.deltaTime * -1;
+        
+    }
+}
---
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/Spaun.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Spaun : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Transform _startPosition;
+    private void Start()
+    {
+        Instantiate(_enemy, _startPosition.position, _startPosition.rotation);
+    }
+
+    
+}
