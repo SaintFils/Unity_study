7e73229695a9725249b733519728e7a1dd6fe52f
Oleg <gurmanoleg@yandex.ru>
2020-11-20 13:14:47 +0300

    Commit for pull request

---
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/CameraMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    //[SerializeField] private float _cameraSpeed = 3.0f;
+
+
+    private void Update()
+    {
+       //transform.position = Vector3.MoveTowards(transform.position, _target.position, _target.position.magnitude);
+        transform.LookAt(_target);        
+    }
+}
--- HomeWorkGameGurmanO/Assets/Trash_Bucket/MoveFirst.cs
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/MoveFirst.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
 
+
 public class MoveFirst : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3;
+    [SerializeField] private float _speed = 3.0f;
+
     private Vector3 _direction = Vector3.zero; 
+
 
      private void Update()
     {
-        _direction.x = -Input.GetAxis("Horizontal"); //модель развернута форвардом назад, поэтому -
+        _direction.x = -Input.GetAxis("Horizontal"); //у ассета форвард направлен назад, поэтому приходится минусовать значение
         _direction.z = -Input.GetAxis("Vertical");
-
-        //transform.position += _direction * Time.deltaTime * _speed;        
+        _direction.Normalize();
     }
+
 
     private void FixedUpdate()
     {
         transform.position += _direction * Time.deltaTime * _speed;
-
     }
 }
