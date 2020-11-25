e46e7e47249b800a36540e0944ee8c872bac31ba
Oleg <gurmanoleg@yandex.ru>
2020-11-25 03:50:37 +0300

    HW 3 process

---
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/MyScripts/DoorButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+public class DoorButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _door = null;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _door.transform.localEulerAngles = new Vector3(0, 270, 0);
+        }
+    }
+}
---
+++ HomeWorkGameGurmanO/Assets/Trash_Bucket/MyScripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _turnSpeed = 3.0f;
+    private float _rotation;
+    private Vector3 _direction = Vector3.zero; 
+
+
+     private void Update()
+    {
+        _rotation = Input.GetAxis("Horizontal"); 
+        _direction.z = Input.GetAxis("Vertical");
+        //_direction.Normalize();
+    }
+
+
+    private void FixedUpdate()
+    {
+        var playerSpeed = _direction * Time.fixedDeltaTime * _speed;
+        transform.Translate(playerSpeed);
+        transform.Rotate(Vector3.up * _rotation * _turnSpeed * Time.fixedDeltaTime);
+    }
+}
